99d4d8db5695cee3c69bfa5d563ffb3c3a2b612c
venrii <francsamonte09@gmail.com>
2024-06-30 03:17:41 +0800

    redo

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -21,7 +21,6 @@ export function Navbar() {
           className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-        <center>
           <div className="flex flex-row space-x-0 pr-10">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
@@ -35,7 +34,6 @@ export function Navbar() {
               )
             })}
           </div>
-          </center>
         </nav>
       </div>
     </aside>
