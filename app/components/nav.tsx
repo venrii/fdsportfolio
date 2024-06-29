20ee7b6cadb0cdf6ccc4f65ac5ee3e45757901d9
venrii <francsamonte09@gmail.com>
2024-06-30 03:15:29 +0800

    div cen

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -16,12 +16,12 @@ const navItems = {
 export function Navbar() {
   return (
     <aside className="-ml-[8px] mb-16 tracking-tight">
-    <center>
       <div className="lg:sticky lg:top-20">
         <nav
           className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
+        <center>
           <div className="flex flex-row space-x-0 pr-10">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
@@ -35,7 +35,7 @@ export function Navbar() {
               )
             })}
           </div>
-        </center>
+          </center>
         </nav>
       </div>
     </aside>
