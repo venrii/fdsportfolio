2507566364404f89bb3ec583a69fbd29f768cf43
venrii <francsamonte09@gmail.com>
2024-06-30 02:15:29 +0800

    fixed paragraphs

--- app/practicum/page.tsx
+++ app/practicum/page.tsx
@@ -6,21 +6,23 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
-          <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Overview of the Practicum Engagement</h1>
-          <p className="mb-4">
-              {`Fasttrack Solutions is a consultancy services company that provides world-class computerized business solutions to Enterprise and SMB markets. 
-                Founded in the Philippines in 2005, Fasttrack Solutions is ONE of the largest business technologies providers in Asia Pacific and is now the 
-                go-to partner in ERP solutions, cloud migration, cloud services, etc., of SMEs and large enterprises.
+        <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Overview of the Practicum Engagement</h1>
+        <p className="mb-4">
+            {`Fasttrack Solutions is a consultancy services company that provides world-class computerized business solutions to Enterprise and SMB markets. 
+            Founded in the Philippines in 2005, Fasttrack Solutions is ONE of the largest business technologies providers in Asia Pacific and is now the 
+            go-to partner in ERP solutions, cloud migration, cloud services, etc., of SMEs and large enterprises.`}
+        </p>
+        <p className="mb-4">
+            {`During my practicum, I was primarily assigned to observe how my supervisors handle and create solutions within the company. My supervisors 
+            provided me with various small tasks every so often to assist with ongoing projects and ensure my active participation in the workflow. 
+            Additionally, I was entrusted with two significant activities that allowed me to apply and enhance my skills in real-world scenarios.`}
+        </p>
+        <p className="mb-4">
+            {`As per requirements, the total hours rendered for my practicum amounted to 324 hours. This comprised 120 hours dedicated to an online program 
+            called the "CODEX Program"", which focused on SQL, Microsoft Software and problem-solving techniques. The remaining 204 hours were spent 
+            in work immersion, where I gained experience in the company's operational environment.`}
 
-                During my practicum, I was primarily assigned to observe how my supervisors handle and create solutions within the company. My supervisors 
-                provided me with various small tasks every so often to assist with ongoing projects and ensure my active participation in the workflow. 
-                Additionally, I was entrusted with two significant activities that allowed me to apply and enhance my skills in real-world scenarios.
-
-                As per requirements, the total hours rendered for my practicum amounted to 324 hours. This comprised 120 hours dedicated to an online program 
-                called the "CODEX Program"", which focused on SQL, Microsoft Software and problem-solving techniques. The remaining 204 hours were spent 
-                in work immersion, where I gained experience in the company's operational environment.`}
-
-          </p>
+        </p>
     </section>
   )
 }
